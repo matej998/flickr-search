697aa112876bb325ffa2fcc98d9c39d608a0d84a
Matej <35317314+matej998@users.noreply.github.com>
2021-01-04 20:55:02 +0100

    Add files via upload

---
+++ js/flickr.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("button").removeClass("selected");
+    $(this).addClass("selected");
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var options = {
+      tags : animal,
+      format : "json"
+    };
+    
+    function displayPhotos(data){
+    var statusHTML = "<ul>";
+      $.each(data.items, function(i, photo){
+      statusHTML += "<li class = 'grid-25 tablet-grid-50'>";
+        statusHTML += "<a href ='" + photo.link + "' class='image'>";
+        statusHTML += "<img src ='" +photo.media.m + "'></a></li>";
+      });
+      statusHTML += "</ul>";
+      $("#photos").html(statusHTML);
+    }
+    $.getJSON(url, options, displayPhotos);
+  });
+});
